26b8c361a7c9601a1c4fef16423f0d564160e662
Danigil06 <danigilmed@gmail.com>
2025-11-14 12:53:04 +0100

    Update PAC-3_Usuaris.sql

--- PAC-3_Usuaris.sql
+++ PAC-3_Usuaris.sql
@@ -1,23 +1,16 @@
--- CREACIÓ DE ROLS
+
 DROP ROLE IF EXISTS `tecnic`, `administratiu`, `professor`;
 
 CREATE ROLE `tecnic`;
 CREATE ROLE `administratiu`;
 CREATE ROLE `professor`;
 
-
--- PRIVILEGIS DEL ROL "tecnic"
 GRANT ALL PRIVILEGES ON `PAC3`.* TO `tecnic`;
 
--- PRIVILEGIS DEL ROL "administratiu"
 GRANT SELECT, INSERT, UPDATE, DELETE ON `PAC3`.* TO `administratiu`;
 
--- PRIVILEGIS DEL ROL "professor"
 GRANT SELECT ON `PAC3`.* TO `professor`;
 GRANT INSERT, UPDATE ON `PAC3`.`Matricula_Assignatura` TO `professor`;
-
--- APLICACIÓ DELS CANVIS
-FLUSH PRIVILEGES;
 
 DROP USER IF EXISTS 'jvancells'@'%';
 DROP USER IF EXISTS 'smoreno'@'%';
